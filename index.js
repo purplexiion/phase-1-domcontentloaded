027aaecd0d3515d1f59184f47d0e251d7270a873
Wamuyu <michellewachira25@gmail.com>
2022-06-14 22:11:59 +0300

    commit tree 25239fb9753070803fc24b9397b9a35cfaa54613

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let p = document.querySelector('p')
+    p.textContent = "This is really cool!"
+})
